5183900e194a588f4fa494ad7d4c204f65f2d102
NickMro <nickmro@gmail.com>
2014-08-17 19:22:20 -0700

    made two week graph

--- app/assets/javascripts/graph.js
+++ app/assets/javascripts/graph.js
@@ -77,6 +77,9 @@ $(document).on('ready page:load', function() {
 	//format of our choosing.
 
 			var dateString = function(date){
+				
+				//In order to get JavaScript month names, we need to set an array
+				//of month names.
 				var monthArray = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 				return date.getDate() + " " + monthArray[date.getMonth()] + " " + date.getFullYear();
@@ -102,9 +105,9 @@ $(document).on('ready page:load', function() {
 			var countTicks = function(data, steps){  //data is the data to pass through, steps indicate intervals
 				var dateTicks = [];
 				var day_to_ms = 1000 * 60 * 60 * 24;
-					for (i = 0; i <= totalDays; ++i){
+					for (i = 0; i <= data.length; ++i){
 						if (i%steps == 0){  //Find out if the day is an even amount of days away from first date
-							dateTicks.push(d3.time.day.offset(firstDate, i))  //If it is, push the date into the ticks array
+							dateTicks.push(d3.time.day.offset(convertDate(data[0].date), i))  //If it is, push the date into the ticks array
 						}
 					}
 					return dateTicks  //Pass the ticks to the tickValues method
@@ -341,124 +344,144 @@ $(document).on('ready page:load', function() {
 			document.getElementById("minValueDate").innerHTML = minDateString;
 
 
-	// ------ History Graph ------ //
-
-			// --- Dimensions of the Graph --- //
-
-			//Get height and width of svg div
-			var w = document.getElementById("questionGraph").offsetWidth;
-			var h = document.getElementById("questionGraph").offsetHeight;
-			var padding = 5;
-			var leftPadding = 30;
-			var bottomPadding = 50;
-			var topPadding = 20;
+	// ------ Previous Two Weeks Graph ------ //
 
 
-			// --- Scales and Axes --- //
+			twoWeekGraph = function(){
 
-			//Set up the x scale so that it includes the days we want it to include
-			var xScale = d3.time.scale()
-						.domain([firstDate, d3.time.day.offset(lastDate, 1)])
-						.rangeRound([0 + leftPadding + padding*8, w]);
+				//Set an empty for where our graph data point objects will go
+				var twoWeekData = [];
 
-			//Set up the y scale so that the maximum is the highest value
-			var yScale = d3.scale.linear()
-						.domain([0, maximumValue])
-						.range([h - bottomPadding, 0 + topPadding]);
+				//Most recent date variable
+				var mostRecentDate = convertDate(data[data.length-1].date);
 
-			//Set up the x axis
-			var xAxis = d3.svg.axis()
-						.scale(xScale)
-						.orient("bottom")
-						.ticks(d3.time.days, 1)
-						.tickFormat(d3.time.format('%d %b'))
-						.tickValues(countTicks(data, Math.ceil(totalDays/10))) //call the ticks function to create ticks
-						.tickSize(0)
-						.tickPadding(8);
+				//Loop through all of our dates, return the ones that occurred with two
+				//weeks of our most recent date and push them to the twoWeekData array
+				for (i = 0; i < data.length; ++i){
+					if (convertDate(data[i].date) > (mostRecentDate - (14 * dayToMs))){
+						twoWeekData.push(data[i])
+					}
+				};
+				console.log(twoWeekData);
 
-			//Set up the y axis
-			var yAxis = d3.svg.axis()
-						.scale(yScale)
-						.orient("left")
-						.ticks(5)
-						.tickSize(0)
-						.tickFormat(function(d){
-							return d + " " + units
-						})
+				// --- Dimensions of the Graph --- //
+
+				//Get height and width of svg div
+				var w = document.getElementById("twoWeekGraph").offsetWidth;
+				var h = document.getElementById("twoWeekGraph").offsetHeight;
+				var padding = 5;
+				var leftPadding = 20;
+				var bottomPadding = 50;
+				var topPadding = 20;
+
+
+				// --- Scales and Axes --- //
+
+				//Set up the x scale so that it includes the days we want it to include
+				var xScale = d3.time.scale()
+							.domain([convertDate(twoWeekData[0].date), d3.time.day.offset(lastDate, 1)])
+							.rangeRound([0 + leftPadding + padding*8, w]);
+
+				//Set up the y scale so that the maximum is the highest value
+				var yScale = d3.scale.linear()
+							.domain([0, maximumValue])
+							.range([h - bottomPadding, 0 + topPadding]);
+
+				//Set up the x axis
+				var xAxis = d3.svg.axis()
+							.scale(xScale)
+							.orient("bottom")
+							.ticks(d3.time.days, 1)
+							.tickFormat(d3.time.format('%d %b'))
+							.tickValues(countTicks(twoWeekData, Math.ceil(twoWeekData.length/10))) //call the ticks function to create ticks
+							.tickSize(0)
+							.tickPadding(8);
+
+				//Set up the y axis
+				var yAxis = d3.svg.axis()
+							.scale(yScale)
+							.orient("left")
+							.ticks(5)
+							.tickSize(0)
+							.tickFormat(function(d){
+								return d + " " + units
+							});
+				
+
+			// ------ Draw the SVG element ------ //
+
+				var svg = d3.select("#twoWeekGraph")
+							.append("svg")
+							.attr("width", w)
+							.attr("height", h);
+
+
 			
 
-	// ------ Draw the SVG element ------ //
+			// ------ Append data to the SVG element ------ //
 
-			var svg = d3.select("#questionGraph")
-						.append("svg")
-						.attr("width", w)
-						.attr("height", h);
+				svg.selectAll("rect")
+							.data(twoWeekData)
+							.enter()
+							.append("rect")
+							
+							// The animation
+							.transition()  //begin the animation
+							.duration(500)  //set the duration of the animation
+							.each("start", function(){ //specifies the starting attributes of the animation
+								d3.select(this)
+									.attr("y", h - bottomPadding)
+									.attr("height", 0)
+									.attr("fill", "rgb(242,233,225)")
+							})
+							
+							//Set starting x position of each bar of the bar graph
+							.attr("x", function(d) {
+								return xScale(convertDate(d.date)); //Starting position depends on the number of days between first and last
+							})
+							
+							//Set the starting y position of each bar
+							.attr("y", function(d) {
+								return yScale(d.value);  //Starting y position needs to be set to the height of the svg element minus the height of the bar 
+							})
 
+							//Set the width of each bar
+							.attr("width", w/(totalDays + 1) - padding)  //Make the width of each bar by dividing them evenly from the width of the svg element and subtracting the padding between each bar
+							
+							//Set the height of each bar
+							.attr("height", function(d) {
+								return yScale(0) - yScale(d.value);
+							})
 
-			
+				//Append the axes
+				svg.append("g")
+					.attr("class", "axis")
+					.attr("transform", "translate(0," + (h - bottomPadding) + ")")
+					.call(xAxis)
+					.selectAll("text")
+						.attr("font-size", 14)
+						.attr("dx", (totalDays < 10 ? ((w/(totalDays + 1))/2 ) : -25) + "px")
+						.attr("dy", (totalDays < 10 ? "10px" : "0"))
+						.attr("transform", function(){
+							return "rotate("+(totalDays < 10 ? 0 : -90)+")"
+						});
 
-	// ------ Append data to the SVG element ------ //
+				svg.append("g")
+					.attr("class", "axis")
+					.attr("transform", "translate("+(padding+leftPadding)+",0)")
+					.call(yAxis)
+					.selectAll("text")
+						.attr("dx", 20)
+						.attr("font-size", 14);
 
-			svg.selectAll("rect")
-						.data(data)
-						.enter()
-						.append("rect")
-						
-						// The animation
-						.transition()  //begin the animation
-						.duration(500)  //set the duration of the animation
-						.each("start", function(){ //specifies the starting attributes of the animation
-							d3.select(this)
-								.attr("y", h - bottomPadding)
-								.attr("height", 0)
-								.attr("fill", "rgb(168,219,168)")
-						})
-						
-						//Set starting x position of each bar of the bar graph
-						.attr("x", function(d) {
-							return xScale(convertDate(d.date)); //Starting position depends on the number of days between first and last
-						})
-						
-						//Set the starting y position of each bar
-						.attr("y", function(d) {
-							return yScale(d.value);  //Starting y position needs to be set to the height of the svg element minus the height of the bar 
-						})
+			};
 
-						//Set the width of each bar
-						.attr("width", w/(totalDays + 1) - padding)  //Make the width of each bar by dividing them evenly from the width of the svg element and subtracting the padding between each bar
-						
-						//Set the height of each bar
-						.attr("height", function(d) {
-							return yScale(0) - yScale(d.value);
-						})
-
-			//Append the axes
-			svg.append("g")
-				.attr("class", "axis")
-				.attr("transform", "translate(0," + (h - bottomPadding) + ")")
-				.call(xAxis)
-				.selectAll("text")
-					.attr("font-size", 14)
-					.attr("dx", (totalDays < 10 ? ((w/(totalDays + 1))/2 ) : -25) + "px")
-					.attr("dy", (totalDays < 10 ? "10px" : (w/(totalDays + 1) - padding*4) + "px"))
-					.attr("transform", function(){
-						return "rotate("+(totalDays < 10 ? 0 : -90)+")"
-					});
-
-			svg.append("g")
-				.attr("class", "axis")
-				.attr("transform", "translate("+(padding+leftPadding)+",0)")
-				.call(yAxis)
-				.selectAll("text")
-					.attr("dx", 20)
-					.attr("font-size", 14);
+			twoWeekGraph();
 
 
 
 	// ------ Populate Data in the Dashboard ------ //
 
-			//In order to get JavaScript month names, we need to set an array
-			//of month names.
 			
 			
 
